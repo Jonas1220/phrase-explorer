88c8def8fcf6cab72d8b14c81eae9b4b64a4779b
Jonas Kaatz <jonas@jonaskaatz.com>
2024-03-08 10:03:33 +0100

    x

--- src/Start/Start.tsx
+++ src/Start/Start.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import data from "../utils/data.json"
-import { Link } from 'react-router-dom';
 import LangItem from '../components/LangItem';
 interface LanguageData {
     [key: string]: {
@@ -33,9 +32,9 @@ export default function Start() {
     return (
         <div className="bg-red-200 w-full flex justify-center">
             <div className='bg-blue-200s w-full max-w-4xl border border-black p-2'>
-                <div className='bg-green-200s w-full flex justify-between'>
-                    <h1>HEADER</h1>
-                    <button>btn</button>
+                <div className='bg-green-200s w-full flex justify-between items-center'>
+                    <h1 className='text-3xl'>Phrase Explorer</h1>
+                    <button className='px-3 py-1 border border-black rounded-full'>login</button>
                 </div>
                 <input className='w-full p-2 rounded-full my-2' type="text" placeholder="Search..." value={filter} onChange={(e) => setFilter(e.target.value)} />
                 {filteredLanguages.map((item, index) => (
--- src/components/LangItem.tsx
+++ src/components/LangItem.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 export default function LangItem({langIso, lang, flag}:{langIso:string, lang:string, flag:string}) {
     return (
-        <Link to={'/'+langIso} className='bg-slate-300 flex justify-between my-2 py-2 px-4 rounded-full border border-black'>
-            <h1 className='text-3xl'>{lang}</h1>
-            <h1 className='text-3xl'>{flag}</h1>
+        <Link to={'/'+langIso} className='bg-slate-300 flex justify-between my-2 py-2 px-4 rounded-full border border-black items-center'>
+            <h1 className='text-2xl sm:text-3xl'>{lang}</h1>
+            <span className={"w-10 h-10 rounded-full fib fis fi-"+langIso}></span>
+            {/* <h1 className='text-3xl'>{flag}</h1> */}
         </Link>
     )
 }

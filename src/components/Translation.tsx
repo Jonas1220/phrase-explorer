f59c6e2ce1670e81962fa99ed88057b5e2111c58
Jonas Kaatz <jonas@jonaskaatz.com>
2024-03-08 16:29:21 +0100

    x

--- src/components/Translation.tsx
+++ src/components/Translation.tsx
@@ -4,7 +4,7 @@ export default function Translation({langIso, input, output}:{langIso:string, in
     return (
         <div className='my-2 flex flex-col items-center'>
             {/* <h1 className='text-xl text-black text-nowrap'>{output.replace(' (','\n\n(')}</h1> */}
-            <h1 className='text-xl text-black self-center' dangerouslySetInnerHTML={{ __html: output.replace(' (','<br>(') }} />
+            <h1 className='text-xl text-black text-center whitespace-pre'>{output}</h1>
             <h1 className='text-sm text-gray-500'>{'('+input+')'}</h1>
         </div>
     )

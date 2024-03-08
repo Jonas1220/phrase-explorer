828d4a14ef84465f7929a006d3c77d50645a3356
Jonas Kaatz <jonas@jonaskaatz.com>
2024-03-08 20:58:24 +0100

    x

--- src/Start/Start.tsx
+++ src/Start/Start.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import data from "../utils/data.json"
 import LangItem from '../components/LangItem';
+import { Link } from 'react-router-dom';
 interface LanguageData {
     [key: string]: {
         phrases: { [key: string]: string };
@@ -31,12 +32,13 @@ export default function Start() {
         <div className="w-full flex flex-col items-center">
             <div className='max-w-2xl w-full flex justify-between items-center my-5 p-2'>
                 <h1 className='text-3xl text-gray-100'>Phrase Explorer</h1>
-                {/* <button className='px-3 py-1 border border-black rounded-full'>login</button> */}
+                <Link to={'https://grnwinlvg3n.typeform.com/to/YWb8qz0I'} target='_blank' className='px-3 py-1 border border-gray-300 rounded-full text-gray-300 hover:text-white hover:border-white'>Add Language</Link>
+
             </div>
             <div className='bg-blue-200s w-full max-w-2xl p-2 px-3 bg-white rounded-3xl'>
                 <input className='w-full p-2 rounded-full my-2 bg-slate-200 border focus:border-indigo-500' type="text" placeholder="Search..." value={filter} onChange={(e) => setFilter(e.target.value)} />
                 {filteredLanguages.map((item, index) => (
-                    <LangItem langIso={item.key} lang={item.val} />
+                    <LangItem key={item.key} langIso={item.key} lang={item.val} />
                 ))}
             </div>
         </div>

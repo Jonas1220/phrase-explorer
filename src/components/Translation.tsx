c52034c10468944bc5ad29009a45bec5ccca95dd
Jonas Kaatz <jonas@jonaskaatz.com>
2024-03-08 16:29:49 +0100

    x

--- src/components/Translation.tsx
+++ src/components/Translation.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Translation({langIso, input, output}:{langIso:string, input:string, output:string}) {
     return (
         <div className='my-2 flex flex-col items-center'>
-            {/* <h1 className='text-xl text-black text-nowrap'>{output.replace(' (','\n\n(')}</h1> */}
             <h1 className='text-xl text-black text-center whitespace-pre'>{output}</h1>
             <h1 className='text-sm text-gray-500'>{'('+input+')'}</h1>
         </div>

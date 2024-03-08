f3bcfd75a84b407a47133a430228d44224be6fde
Jonas Kaatz <jonas@jonaskaatz.com>
2024-03-08 09:38:57 +0100

    x

--- src/Start/Start.tsx
+++ src/Start/Start.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import data from "../utils/data.json"
 import { Link } from 'react-router-dom';
+import LangItem from '../components/LangItem';
 interface LanguageData {
     [key: string]: {
         phrases: { [key: string]: string };
@@ -11,21 +12,18 @@ interface LanguageData {
 interface LanguageArrayItem {
     key: string;
     val: string;
+    flag: string
 };
 
-const createLanguageArray = (data: LanguageData): { key: string; val: string }[] => {
+const createLanguageArray = (data: LanguageData): { key: string; val: string; flag: string }[] => {
     return Object.keys(data).map((key) => ({
         key,
-        val: `${data[key].full} ${data[key].flag}`
+        val: data[key].full,
+        flag: data[key].flag
     }));
 };
 
 const languageArray: LanguageArrayItem[] = createLanguageArray(data);
-
-const lang= 'de';
-
-console.log(data[lang]);
-
 
 export default function Start() {
     const [filter, setFilter] = useState('');
@@ -34,16 +32,17 @@ export default function Start() {
         (item) => item.val.toLowerCase().includes(filter.toLowerCase())
     );
     return (
-        <div className="bg-red-400">
-            <input type="text" placeholder="Search..." value={filter} onChange={(e) => setFilter(e.target.value)} />
-            <ul>
+        <div className="bg-red-200 w-full flex justify-center">
+            <div className='bg-blue-200s w-full max-w-4xl border border-black p-2'>
+                <div className='bg-green-200s w-full flex justify-between'>
+                    <h1>HEADER</h1>
+                    <button>btn</button>
+                </div>
+                <input type="text" placeholder="Search..." value={filter} onChange={(e) => setFilter(e.target.value)} />
                 {filteredLanguages.map((item, index) => (
-                    <li key={item.key}>
-                        <Link to={"/"+item.key}>{item.val}</Link>
-                    </li>
+                    <LangItem langIso={item.key} lang={item.val} flag={item.flag}/>
                 ))}
-            </ul>
+            </div>
         </div>
     )
 }
-// Turkish, swedish, german, spanish, italian, french, dutch, Portuguese, Arabic, Chinese (Mandarin), Russian, Japanese, Hindi, Korean
---
+++ src/components/LangItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function LangItem({langIso, lang, flag}:{langIso:string, lang:string, flag:string}) {
+    return (
+        <Link to={'/'+langIso} className='bg-slate-300 flex justify-between my-2 p-2 rounded border border-black'>
+            <h1 className='text-3xl'>{lang}</h1>
+            <h1 className='text-3xl'>{flag}</h1>
+        </Link>
+    )
+}
